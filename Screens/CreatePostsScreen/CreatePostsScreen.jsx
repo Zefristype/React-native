c7aeff82cf14962726f7da251a2fbc9f74b087af
Zefristype <Rojdion.a@gmail.com>
2023-07-16 23:35:07 +0200

    added load images from gallery

--- Screens/CreatePostsScreen/CreatePostsScreen.jsx
+++ Screens/CreatePostsScreen/CreatePostsScreen.jsx
@@ -1,36 +1,147 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Image,
+} from "react-native";
 import { Feather, FontAwesome } from "@expo/vector-icons";
 import { TextInput } from "react-native-gesture-handler";
+import { useEffect, useState, useRef } from "react";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import * as ImagePicker from "expo-image-picker";
+
 const CreatePostsScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [photo, setPhoto] = useState(null);
+  const [title, setTitle] = useState("");
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  useEffect(() => {
+    const showKeyboard = Keyboard.addListener("keyboardDidShow", () => {});
+    const hideKeyboard = Keyboard.addListener("keyboardDidHide", () => {});
+
+    return () => {
+      hideKeyboard.remove();
+      showKeyboard.remove();
+    };
+  }, []);
+
+  const pressHandler = () => {
+    Keyboard.dismiss();
+  };
+  const takePhoto = async () => {
+    if (cameraRef) {
+      const { uri } = await cameraRef.takePictureAsync();
+      await MediaLibrary.createAssetAsync(uri);
+      setPhoto(uri);
+    }
+  };
+  const onSubmit = () => {
+    if (!photo || !title || !location) {
+      console.log("Not all");
+    } else {
+      console.log("Well done");
+    }
+  };
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.wrapper}>
-        <View style={styles.imageWrapper}>
-          <TouchableOpacity style={styles.circle}>
-            <FontAwesome color={"#BDBDBD"} name="camera" size={24} />
+    <TouchableWithoutFeedback onPress={pressHandler}>
+      <View style={styles.container}>
+        <View style={styles.wrapper}>
+          <Camera style={styles.camera} type={type} ref={setCameraRef}>
+            <Image source={{ uri: photo }} style={styles.photo} />
+            <TouchableOpacity style={styles.circle} onPress={takePhoto}>
+              <FontAwesome color={"#FFFFFF"} name="camera" size={24} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.flipContainer}
+              onPress={() => {
+                setType(
+                  type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back
+                );
+              }}
+            >
+              <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+                {" "}
+                Flip{" "}
+              </Text>
+            </TouchableOpacity>
+          </Camera>
+
+          <TouchableOpacity
+            onPress={async () => {
+              const { status } =
+                await ImagePicker.requestMediaLibraryPermissionsAsync();
+              if (status !== "granted") {
+                console.log("Permission denied");
+                return;
+              }
+
+              const result = await ImagePicker.launchImageLibraryAsync();
+              if (!result.canceled && result.assets.length > 0) {
+                const selectedAsset = result.assets[0];
+                setPhoto(selectedAsset.uri);
+              }
+            }}
+          >
+            <Text style={{ ...styles.text, marginBottom: 32 }}>Load image</Text>
+          </TouchableOpacity>
+          <TextInput
+            onChangeText={setTitle}
+            value={title}
+            style={{ ...styles.input, marginBottom: 16 }}
+            placeholder="Title"
+            placeholderTextColor={"#BDBDBD"}
+          />
+          <View style={styles.locationBox}>
+            <Feather
+              name="map-pin"
+              color={"#BDBDBD"}
+              style={{ marginLeft: 25 }}
+              size={24}
+            />
+            <TextInput
+              onChangeText={setLocation}
+              value={location}
+              style={{ ...styles.input, marginLeft: 4 }}
+              placeholder="Location"
+              placeholderTextColor={"#BDBDBD"}
+            />
+          </View>
+          <TouchableOpacity style={styles.button} onPress={onSubmit}>
+            <Text style={styles.text}>Publish</Text>
           </TouchableOpacity>
         </View>
-        <Text style={{ ...styles.text, marginBottom: 32 }}>Load image</Text>
-        <TextInput
-          style={{ ...styles.input, marginBottom: 16 }}
-          placeholder="Title"
-          placeholderTextColor={"#BDBDBD"}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Location"
-          placeholderTextColor={"#BDBDBD"}
-        />
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>Publish</Text>
+        <TouchableOpacity style={styles.buttonIcon}>
+          <Text>
+            <Feather color={"#BDBDBD"} size={24} name="trash-2" />
+          </Text>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.buttonIcon}>
-        <Text>
-          <Feather color={"#BDBDBD"} size={24} name="trash-2" />
-        </Text>
-      </TouchableOpacity>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -48,18 +159,26 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
   },
-  imageWrapper: {
+  camera: {
+    position: "relative",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     width: "100%",
     height: 240,
-    backgroundColor: "#F6F6F6",
     borderWidth: 1,
     borderColor: "#E8E8E8",
     borderRadius: 8,
     marginTop: 32,
     marginBottom: 8,
+  },
+  photo: {
+    // zIndex: 10000,
+    position: "absolute",
+    // top: 0,
+    // left: 0,
+    width: "100%",
+    height: 240,
   },
   circle: {
     display: "flex",
@@ -68,8 +187,9 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     borderRadius: 60 / 2,
-    backgroundColor: "transparent",
+    backgroundColor: "rgba(255, 255, 255, 0.30)",
   },
+  flipContainer: { position: "absolute", bottom: 10, right: 10 },
   input: {
     width: "100%",
     height: 50,
@@ -98,6 +218,12 @@ const styles = StyleSheet.create({
     height: 40,
     borderRadius: 20,
     marginBottom: 34,
+  },
+  locationBox: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
 
